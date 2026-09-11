befb2544ff6fa1bf69e3f9347e079a0142ebaf3a
criolys <criolys@gmail.com>
2026-09-11 14:41:54 +0200

    feat: finished api server, need tests

--- backend/api-server/src/main.py
+++ backend/api-server/src/main.py
@@ -1,6 +1,6 @@
 import asyncio
 import logging
-from decimal import Decimal
+from datetime import datetime, timedelta, timezone
 from contextlib import asynccontextmanager
 from typing import List, Set
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Depends
@@ -9,7 +9,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 import redis.asyncio as aredis
 
 from database import Ticker, MarketCandle, get_async_session
-from schema import TickerValidation, IndicatorValidation
+from schema import CandleValidation
 from config import REDIS_PORT, REDIS_HOST, REDIS_PASSWORD
 
 logging.basicConfig(
@@ -61,7 +61,7 @@ async def   lifespan(app: FastAPI):
     # Redis listening to ticks (Background Task)
     async def redis_listener():
         pubsub = redis_client.pubsub()
-        await pubsub.subscribe("market_ticks", "market_indicators")
+        await pubsub.subscribe("market_ticks", "market_candles")
 
         try:
             # Waiting for the messages sent by the ingestion or Celery
@@ -83,42 +83,82 @@ async def   lifespan(app: FastAPI):
     listener_task.cancel()
     await app.state.redis_pool.disconnect()
 
-app = FastAPI(title="Market Data API", lifespan=lifespan)
+app = FastAPI(title="Stock Market Data API", lifespan=lifespan)
 
 # For watchlist
 @app.get("/api/markets")
-async def   get_watchlist(
-    symbol: str,
-    last_price: Decimal,
-    last_day_change: Decimal,
-    session: AsyncSession = Depends(get_async_session)
-):
-    """Gets the latest price of the currency and compare it to the latest price from 24-hrs ago"""
-    pass
+async def   get_watchlist(session: AsyncSession = Depends(get_async_session)):
+    """Gets the latest price of the currency and compare it to the price from 24-hrs ago"""
+
+    now = datetime.now(timezone.utc)
+    day_ago = now - timedelta(hours=24)
+
+    symbols_query = select(Ticker.symbol).distinct()
+    symbols_res = await session.execute(symbols_query)
+    symbols = symbols_res.scalars().all()
+
+    watchlist = []
+
+    for symbol in symbols:
+        # Getting the latest price of each currency
+        latest_query = (
+            select(Ticker.price)
+            .where(Ticker.symbol == symbol)
+            .order_by(Ticker.timestamp.desc())
+            .limit(1)
+        )
+        latest_price = await (session.execute(latest_query)).scalar()
+
+        # Getting the oldest price (24-hrs ago) of each currency
+        old_query = (
+            select(Ticker.price)
+            .where(Ticker.symbol == symbol, Ticker.timestamp <= day_ago)
+            .order_by(Ticker.timestamp.desc())
+            .limit(1)
+        )
+        old_price = await (session.execute(old_query)).scalar()
+
+        # Calculating price variation in percentage
+        if latest_price and old_price and old_price > 0:
+            change_24h = ((latest_price - old_price) / old_price) * 100
+        else:
+            change_24h = 0.0
+
+        watchlist.append({
+            "symbol": symbol,
+            "lastPrice": latest_price,
+            "change24h": change_24h
+        })
+
+    return watchlist
 
 # HTTP Ticker, main graph (Database)
-@app.get("/api/markets/{symbol}/candles", response_model=List[TickerValidation])
-async def   get_historical_ticks(
+@app.get("/api/markets/{symbol}/candles", response_model=List[CandleValidation])
+async def   get_candles(
     symbol: str,
+    interval: str = "1m",
     limit: int = 100,
     session: AsyncSession = Depends(get_async_session)
 ):
-    """Collect the last x ticks (default 100) from the table "market_ticks"."""
+    """Collect the last x ticks (default 100) from the table "market_candles" with a given interval (default 1-min)"""
 
     query = (
-        select(Ticker)
-        .where(Ticker.symbol == symbol.upper())
-        .order_by(Ticker.timestamp.desc())
+        select(MarketCandle)
+        .where(MarketCandle.symbol == symbol.upper(),
+               MarketCandle.interval == interval
+        )
+        .order_by(MarketCandle.time.desc())
         .limit(limit)
     )
 
     # Getting tuple with result -> unpack it with .scalars() -> putting them in a list with .all()
     result = await session.execute(query)
-    ticks = result.scalars().all()
-    return ticks
+    candles = result.scalars().all()
+    # Returning all the tables but backwards (from the oldest to the latest)
+    return candles[::-1]
 
 # WebSocket (Cache)
-@app.websoclets("/ws/markets/{symbol}")
+@app.websockets("/ws/markets/{symbol}")
 async def   ws_market_data(websocket: WebSocket):
     """Opening a websocket pipeline and pushing ticks from Redis (every s/ms)"""
 
